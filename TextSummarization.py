5ff20dd199516db38b0fa31fb962d4aa990cb92e
Krzysiek <krzysiek.dziedzic@outlook.com>
2018-06-23 02:10:55 +0200

    work for english newspapers and polish articles which have "article" section on the website

--- TextSummarization.py
+++ TextSummarization.py
@@ -6,37 +6,56 @@ from string import punctuation
 from nltk.probability import FreqDist
 from heapq import nlargest
 from collections import defaultdict
+from langdetect import detect
 
-articleURL = "http://antyweb.pl/mysz-cooler-master-mastermouse-520/"
+articleURL = "http://antyweb.pl/elektronika-ograniczone-zaufanie/"
 
 
 def getTextWaPo(url):
-    req = Request(articleURL,headers={'User-Agent': 'Opera/53.0.2907.99'})
+    req = Request(articleURL, headers={'User-Agent': 'Opera/53.0.2907.99'})
     page = urlopen(req).read().decode('utf8')
     soup = BeautifulSoup(page, "lxml")
-    text = ' '.join(map(lambda p: p.text, soup.find_all("article")))
-    return text.encode('utf8', errors='replace').decode().replace("?"," ")
+    text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
+    if text:
+        return text.encode('utf-8', errors='replace').decode().replace("\n", "")
+    else:
+        page = urlopen(articleURL).read().decode('utf8')
+        soup = BeautifulSoup(page, "lxml")
+        text = ' '.join(map(lambda p: p.text, soup.find_all('article')))
+        return text.encode('utf-8', errors='replace').decode().strip()
 
 
 text = getTextWaPo(articleURL)
 
 
 def summarize(text, n):
-    sents = sent_tokenize(text)
-    word_sent = word_tokenize(text.lower())
-    _stopword = set(stopwords.words('polish.txt') + list(punctuation))
-    word_sent = [word for word in word_sent if word not in _stopword]
-    freq = FreqDist(word_sent)
-    ranking = defaultdict(int)
+    if text:
+        sents = sent_tokenize(text)
+        sents = sents[1:-1]
 
-    for i, sent in enumerate(sents):
-        for w in word_tokenize(sent.lower()):
-            if w in freq:
-                ranking[i] += freq[w]
+        assert n < len(sents), "The number of sentences that you want in the summary is too high from a possible"
 
-    sents_idx = nlargest(n, ranking, key=ranking.get)
-    return [sents[j] for j in sorted(sents_idx)]
+        word_sent = word_tokenize(text.lower())
+
+        if detect(sents[3]) == "en":
+            _stopword = set(stopwords.words('english') + list(punctuation))
+        else:
+            _stopword = set(stopwords.words('polish.txt') + list(punctuation))
+
+        word_sent = [word for word in word_sent if word not in _stopword]
+
+        freq = FreqDist(word_sent)
+        ranking = defaultdict(int)
+
+        for i, sent in enumerate(sents):
+            for w in word_tokenize(sent.lower()):
+                if w in freq:
+                    ranking[i] += freq[w]
+
+        sents_idx = nlargest(n, ranking, key=ranking.get)
+        return [sents[j] for j in sorted(sents_idx)]
+    else:
+        return "Empty \"div id=\"article\""
 
 
-print(summarize(text, 3))
-
+print(summarize(text, 4))
