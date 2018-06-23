10488b3786cec46375c67b929f8f5b3f0fcf6262
Piotr Harasiuk <pharasiuk@gmail.com>
2018-06-23 18:39:24 +0200

    Add new logo and fix loading static content

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
 
 # importing views
 # we need to create views.py
@@ -29,4 +31,4 @@ urlpatterns = [
     url(r'^shortenArticle/', views.index),
     # main page
     url(r'^$', views.mainPage),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
